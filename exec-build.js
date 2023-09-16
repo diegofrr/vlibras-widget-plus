d9555c04406d232014d3c8653279e4e63cb10ffd
Diêgo Ferreira <diegofrr.19@gmail.com>
2023-09-16 14:37:46 -0300

    update

--- exec-build.js
+++ exec-build.js
@@ -1,10 +1,9 @@
 const BUILD_URL = 'https://raw.githubusercontent.com/diegofrr/vlibras-widget-plus/main/vlibras-plugin.js'
 
-root = ['[vp', '[vw', '.vpw-', '.vp-']
+root = ['[vp', '[vw', '.vpw-', '.vp-', '.vw-']
 
 document.querySelectorAll('style').forEach(style => {
     if (root.some(i => style.textContent.includes(i))) style.remove()
 })
-
 
 fetch(BUILD_URL).then(res => res.text()).then(js => eval(js))
